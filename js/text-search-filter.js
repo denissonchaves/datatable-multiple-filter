6f166562fe598b2fdc5aa6373dbe87609b415556
Denisson Chaves <93844216+denissonchaves@users.noreply.github.com>
2022-06-10 02:02:55 -0300

    fix: filtro de pesquisa tipo text + select

--- js/text-search-filter.js
+++ js/text-search-filter.js
@@ -31,29 +31,27 @@ $(document).ready(function () {
 
       });
 
+      this.api().columns([2]).every(function () {
+        var column = this;
+        var select = $('<select><option value=""></option></select>')
+          .appendTo($(column.footer()).empty())
+          .on('change', function () {
+            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+            column.search(val ? '^' + val + '$' : '', true, false).draw();
+          });
+
+        column
+          .data()
+          .unique()
+          .sort()
+          .each(function (d, j) {
+            select.append('<option value="' + d + '">' + d + '</option>');
+          });
+      });
+
     },
 
-    initComplete: function () {
-      this.api().columns([2]).every(function () {
-          var column = this;
-          var select = $('<select><option value=""></option></select>')
-            .appendTo($(column.footer()).empty())
-            .on('change', function () {
-              var val = $.fn.dataTable.util.escapeRegex($(this).val());
-  
-              column.search(val ? '^' + val + '$' : '', true, false).draw();
-            });
-  
-          column
-            .data()
-            .unique()
-            .sort()
-            .each(function (d, j) {
-              select.append('<option value="' + d + '">' + d + '</option>');
-            });
-      });
-    }
-    
   });
   
 });
